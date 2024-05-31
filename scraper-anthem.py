887de80a6f82613923f7ed1b70e44e055d6cb3be
SirADN <adrien.bourgain@student-cs.fr>
2024-05-31 16:12:35 -0700

    ⚡️(update) visit all links

--- scraper-anthem.py
+++ scraper-anthem.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+BASE_URL = "https://www.anthem.com"
 URL = "https://www.anthem.com/ca/provider/policies/clinical-guidelines/updates/"
 
 
@@ -18,6 +19,7 @@ class AnthemScraper:
 
     def __init__(self, headful: bool = False):
         self.url = URL
+        self.base_url = BASE_URL
         self.headless = not headful
         self.driver = self.setup_driver(headful)
 
@@ -63,18 +65,52 @@ class AnthemScraper:
         option.click()
         time.sleep(self.get_random_wait_time())  # Wait for any updates
 
-    def get_results(self):
-        """Get the results from the current page."""
+    def get_num_results(self):
+        """Get the number of results displayed on the page."""
         result_msg = (
             WebDriverWait(self.driver, self.get_random_wait_time())
             .until(
                 EC.visibility_of_element_located(
-                    (By.XPATH, "//div[contains(@class, 'pretend-pagination')]/span")
+                    (
+                        By.XPATH,
+                        "//div[contains(@class, 'pretend-pagination')]/span",
+                    )
                 )
             )
             .text
         )
-        print(result_msg)
+        num_results = int(result_msg.split(" ")[-1])
+        print(f"Number of results: {num_results}")
+        return num_results
+
+    def get_item_links(self):
+        """Get the link to each item on the page."""
+        item_links = []
+        items = self.driver.find_elements(
+            By.CSS_SELECTOR, ".news-item-wrapper .article-headline a"
+        )
+        for item in items:
+            link = item.get_attribute("href")
+            if link.startswith("/"):
+                link = self.base_url + link
+            item_links.append(link)
+        return item_links
+
+    def visit_item_pages(self, item_links):
+        """Visit each item page and process it as needed."""
+        main_window = self.driver.current_window_handle  # Store the main window handle
+        for link in item_links:
+            self.driver.execute_script("window.open('');")  # Open a new tab
+            self.driver.switch_to.window(
+                self.driver.window_handles[1]
+            )  # Switch to the new tab
+            self.driver.get(link)
+            time.sleep(self.get_random_wait_time())  # Allow page to load
+            # Process the item page as needed
+            print(f"Visited: {link}")
+
+            self.driver.close()  # Close the current tab
+            self.driver.switch_to.window(main_window)  # Switch back to the main window
 
     def navigate_next_page(self):
         """Navigate to the next page."""
@@ -93,14 +129,19 @@ class AnthemScraper:
         self.close_popup()
         self.select_filter("formsDocTypeFilter", "medicalpolicy")
         self.select_filter("categoryFilter", "surgery")
+        num_results = self.get_num_results()
 
+        visited_links = set()
         while True:
-            self.get_results()
+            item_links = self.get_item_links()
+            visited_links.update(item_links)
+            self.visit_item_pages(item_links)
             try:
                 self.navigate_next_page()
             except TimeoutException:
                 print("No more pages or next page button not found.")
                 break
+        assert len(visited_links) == num_results, "Some items were not visited."
         self.driver.quit()
 
 
